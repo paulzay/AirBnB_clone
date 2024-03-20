02e168044ea0d12bd0a2ccd35ca07f04222d9959
Paul Ogolla <paulotieno2@gmail.com>
2024-03-20 12:39:04 +0300

    updates

--- models/base_model.py
+++ models/base_model.py
@@ -2,35 +2,36 @@
 
 import uuid
 from datetime import datetime
+""" class definition"""
 
 
 class BaseModel:
+	"""base model class"""
+	def __init__(self, *args, **kwargs):
+		""" Initialize """
+		if not kwargs:
+			self.id = str(uuid.uuid4())
+			self.created_at = datetime.now()
+			self.updated_at = datetime.now()
+		else:
+			for key, value in kwargs.items():
+				if key == "id":
+					self.id = value
+				elif key == "created_at":
+					self.created_at = datetime.fromisoformat(value)
+				elif key == "updated_at":
+					self.updated_at = datetime.fromisoformat(value)
 
-    def __init__(self, *args, **kwargs):
-    	if not kwargs:
-    		self.id = str(uuid.uuid4())
-    		self.created_at = datetime.now()
-    		self.updated_at = datetime.now()
-    	else:
-            for i in kwargs:
-            	if i == "id":
-            		self.id = kwargs[i]
-            	elif i == "created_at":
-            		self.created_at = kwargs[i]
-            	elif i == "updated_at":
-            		self.updated_at = kwargs[i]
+	def __str__(self):
+		class_name = self.__class__.__name__
+		return f"[{class_name}] ({self.id}) {self.__dict__}"
 
+	def save(self):
+		self.updated_at = datetime.now()
 
-    def __str__(self):
-        class_name = self.__class__.__name__
-        return f"[{class_name}] ({self.id}) {self.__dict__}"
-
-    def save(self):
-        self.updated_at = datetime.now()
-
-    def to_dict(self):
-        new_dict = self.__dict__.copy()
-        new_dict['__class__'] = self.__class__.__name__
-        new_dict['created_at'] = self.created_at.isoformat()
-        new_dict['updated_at'] = self.updated_at.isoformat()
-        return new_dict
+	def to_dict(self):
+		new_dict = self.__dict__.copy()
+		new_dict['__class__'] = self.__class__.__name__
+		new_dict['created_at'] = self.created_at.isoformat()
+		new_dict['updated_at'] = self.updated_at.isoformat()
+		return new_dict
