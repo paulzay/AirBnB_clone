b425b0788eb53a0c1aeef81e585d749078d19ae6
Paul Ogolla <paulotieno2@gmail.com>
2024-03-20 12:21:22 +0300

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -1,20 +1,36 @@
 #!/usr/bin/python3
 
 import uuid
-import datetime
+from datetime import datetime
+
 
 class BaseModel:
 
-    def __init__(self, id, created_at, updated_at):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+    def __init__(self, *args, **kwargs):
+    	if not kwargs:
+    		self.id = str(uuid.uuid4())
+    		self.created_at = datetime.now()
+    		self.updated_at = datetime.now()
+    	else:
+            for i in kwargs:
+            	if i == "id":
+            		self.id = kwargs[i]
+            	elif i == "created_at":
+            		self.created_at = kwargs[i]
+            	elif i == "updated_at":
+            		self.updated_at = kwargs[i]
+
 
     def __str__(self):
-        return("[Rectangle] ({}) {}.format(self.id, self.__dict__")
-    
+        class_name = self.__class__.__name__
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
+
     def save(self):
-        pass
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        pass
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
