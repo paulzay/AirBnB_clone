33936aecbe52b7c2e02c35107850337c4ec5a3dd
Paul Ogolla <paulotieno2@gmail.com>
2024-03-24 22:08:50 +0300

    test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,9 @@
+#!/usr/bin/python3
+
+""" test class definition """
 import unittest
 from models.base_model import BaseModel
 
+
 class BaseModelTest(unittest.TestCase):
-    
+    """docstring for class"""
