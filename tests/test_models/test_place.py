36017aaeae7f516f2690ccedbabc79ba0c7598f0
Paul Ogolla <paulotieno2@gmail.com>
2024-03-25 23:33:17 +0300

    testfiles

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" test class definition """
+import unittest
+
+
+class PlaceTest(unittest.TestCase):
+    def test_someting(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_models/test_review.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" test class definition """
+import unittest
+
+
+class ReviewTest(unittest.TestCase):
+    def test_something(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
