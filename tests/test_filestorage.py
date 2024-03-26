b13a5133d7f8e8f62d4cad94e0076726d20cdd00
Paul Ogolla <paulotieno2@gmail.com>
2024-03-26 08:47:39 +0300

    filestorage testfile

---
+++ tests/test_filestorage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+""" test class definition """
+import unittest
+from models.engine.file_storage import FileStorage
+
+
+class ConsoleTest(unittest.TestCase):
+    def test_objects(self):
+        pass
+
+    def test_all(self):
+        pass
+
+    def test_new(self):
+        pass
+
+    def test_save(self):
+        pass
+
+    def test_reload(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
